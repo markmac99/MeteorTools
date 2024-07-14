144eae681307c7572471a50bb4d88a5d06a3997c
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-07-14 17:57:45 +0100

    increase time window to catch more data

--- analysis/fireballAnalyser.ps1
+++ analysis/fireballAnalyser.ps1
@@ -36,7 +36,7 @@ $env:pythonpath="$wmplloc"
 #$solver = read-host -prompt "ECSV or RMS solver? (E/R)"
 $solver = 'E'
 if ($solver -eq 'E') {
-    python -m wmpl.Formats.ECSV . -l -x -r $mcrun -w -t 5
+    python -m wmpl.Formats.ECSV . -l -x -r $mcrun -w -t 15
 }
 else {
     python -m wmpl.Trajectory.CorrelateRMS . -l 
