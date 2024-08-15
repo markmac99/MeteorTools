3feda60f4f1e606322892d4f34cfb1b263f5c637
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-08-16 00:08:14 +0100

    feature to upload videos on demand

---
+++ analysis/addVidorImg.ps1
@@ -0,0 +1,58 @@
+# Copyright (C) 2018-2023 Mark McIntyre 
+#
+# powershell script to upload an extra video or image of an event, given a trajectory ID
+
+$loc = get-location
+if ($args.count -lt 1) {
+    write-output "Usage: addVideoorImg.ps1 trajpath"
+    exit
+}else {
+    $pth = $args[0]
+}
+set-location $PSScriptRoot
+# load the helper functions
+. .\helperfunctions.ps1
+$ini=get-inicontent analysis.ini
+Set-Location $Loc
+$traj = (split-path -leaf $pth)
+
+$yr=$traj.Substring(0,4)
+$ym=$traj.Substring(0,6)
+$ymd=$traj.Substring(0,8)
+
+aws s3 sync s3://ukmda-shared/matches/RMSCorrelate/trajectories/${yr}/${ym}/${ymd}/${traj}/ ${pth} --exclude "*" --include "*.pickle"
+aws s3 sync s3://ukmda-website/reports/${yr}/orbits/${ym}/${ymd}/${traj}/ ${pth} --exclude "*" --include "extra*.html"
+
+$pref = '<a href="'
+$mid = '"><video width="20%"><source src="'
+$tail = '" width="20%" type="video/mp4"></video></a>'
+
+$fils=(get-childitem $pth/*.mp4)
+foreach ($fil in $fils){
+    $fnam = $fil.name
+    $imgpth = "/img/mp4/${yr}/${ym}/${fnam}"
+    Write-Output ${pref}${imgpth}${mid}${imgpth}${tail} >> $pth\extrampgs.html
+    aws s3 cp ${fil} s3://ukmda-website${imgpth}
+}
+$fils=(get-childitem $pth/*.mov)
+foreach ($fil in $fils){
+    $fnam = $fil.name
+    $imgpth = "/img/mp4/${yr}/${ym}/${fnam}"
+    Write-Output ${pref}${imgpth}${mid}${imgpth}${tail} >> $pth\extrampgs.html
+    aws s3 cp ${fil} s3://ukmda-website${imgpth}
+}
+$pref = '<a href="'
+$mid = '"><img src="'
+$tail = '" width="20%"></a>'
+$fils=(get-childitem $pth/*.jpg)
+foreach ($fil in $fils){
+    $fnam = $fil.name
+    Write-Output /img/single/${yr}/${ym}/$fnam >> $pth\extrajpgs.html
+    aws s3 cp ${fil} s3://ukmda-website${imgpth}
+}
+
+aws s3 sync ${pth} s3://ukmda-website/reports/${yr}/orbits/${ym}/${ymd}/${traj}/  --exclude "*" --include "extra*.html"
+$pickle = (get-childitem $pth/*.pickle).fullname
+aws s3 cp ${pickle} s3://ukmda-shared/matches/RMSCorrelate/trajectories/${yr}/${ym}/${ymd}/${traj}/ 
+Write-Output "uploaded $pickle at $(get-date)"
+set-location $loc
